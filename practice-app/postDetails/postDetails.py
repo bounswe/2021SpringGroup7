d2b81c820c5c9af805d4fa5a27dfb619416b9577
mrabiabrn <merverabiabarin@gmail.com>
2021-05-28 01:46:07 +0300

    added edit post functionality

--- practice-app/postDetails/postDetails.py
+++ practice-app/postDetails/postDetails.py
@@ -1,16 +1,34 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+#from werkzeug.wrappers import request
 from ..database import mongo
+
+import datetime 
 
 postDetails_bp = Blueprint('Post Details', __name__)
 
-
-@postDetails_bp.route('/postDetail/<int:postId>', methods=['GET'])
-def getPostDetails(postId):
+# TODO: pass mongo db id of the post
+@postDetails_bp.route('/api/postDetail/<int:postId>', methods=['GET', 'POST'])
+def postDetails(postId):
     
     db = mongo.db
-    dbresponse = db.posts.find({'id': postId}, {'_id': False})
+    dbresponse = db.posts.find({'id': postId},  {'_id': False})
 
     if not dbresponse:
         return 'Post not found xd'
 
+    # update post
+    if request.method == 'POST':
+        editDetails = request.json
+
+        query = { 'id' : postId }
+
+        editDetails['lastEdit'] = datetime.datetime.now()
+        editValues = { '$set': editDetails }
+
+        db.posts.update_one(query, editValues, upsert=False)
+
+        return jsonify(list(db.posts.find({ 'id' : postId }, {'_id': False})))  # updated value
+
+    
     return jsonify(list(dbresponse))
+
