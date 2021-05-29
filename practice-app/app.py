760c0cc4eb8f245300eaed25eaee93af234e5afe
asi345 <inanata15@gmail.com>
2021-05-29 15:36:48 +0300

    added google maps api

--- practice-app/app.py
+++ practice-app/app.py
@@ -47,9 +47,9 @@ db.users.insert_one({
 def index():
     return jsonify({'Uygulama calisiyor mu': 'evet', 'En iyi grup': 'Grup 7'})
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Task was not found'}), 404)
+#@app.errorhandler(404)
+#def not_found(error):
+#    return make_response(jsonify({'error': 'Task was not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- practice-app/profile/profile.py
+++ practice-app/profile/profile.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, abort
 from ..database import mongo
+import requests
 
 profile_bp = Blueprint('User Profiles', __name__)
 
@@ -14,10 +15,20 @@ def getProfile(username):
 
     nofReq = len(curUser['followRequests'])
     nofFollowers = len(curUser['followers'])
-    nofFollowings = len(curUser['followings'])
+    nofFollowings = len(curUser['followings']) 
+
+    key = '&key=AIzaSyAhLt4H3iJLpmC0z_ospp1eMuW1efmqJU0'
+
+    searchBase = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' 
+    placeSearch = requests.get(searchBase + curUser['location'] + key)
+
+    detailsBase = 'https://maps.googleapis.com/maps/api/place/details/json?place_id='
+    placeId = placeSearch.json()['results'][0]['place_id']
+    details = requests.get(detailsBase + placeId + key)
 
     curUser.update({'nofRequests':nofReq, 'nofFollowers':nofFollowers,
-        'nofFollowings':nofFollowings})
+        'nofFollowings':nofFollowings, 'locationmap':placeSearch.json(),
+        'locationdetails':details.json()})
 
     return jsonify(curUser)
 
@@ -45,10 +56,27 @@ def updateProfile(username):
     birthday = curUser['birthday'] if (birthday == '') else birthday
     isVisible = curUser['isVisible'] if (isVisible == '') else isVisible
 
+    nofReq = len(curUser['followRequests'])
+    nofFollowers = len(curUser['followers'])
+    nofFollowings = len(curUser['followings'])
+
+    key = '&key=AIzaSyAhLt4H3iJLpmC0z_ospp1eMuW1efmqJU0'
+
+    searchBase = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' 
+    placeSearch = requests.get(searchBase + location + key)
+
+    detailsBase = 'https://maps.googleapis.com/maps/api/place/details/json?place_id='
+    placeId = placeSearch.json()['results'][0]['place_id']
+    details = requests.get(detailsBase + placeId + key)
+
     updated = {'$set': {'first_name':first_name, 'last_name':last_name, 'email':email,
         'location':location, 'birthday':birthday, 'isVisible':isVisible}}
 
     db.users.update_one(curUser, updated)
     curUser = db.users.find_one({'username': username}, {'_id': False})
+
+    curUser.update({'nofRequests':nofReq, 'nofFollowers':nofFollowers,
+        'nofFollowings':nofFollowings, 'locationmap':placeSearch.json(),
+        'locationdetails':details.json()})
 
     return jsonify(curUser)
