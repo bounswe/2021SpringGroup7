e17721dfc2ab83b29964e951970b460489de8ac4
Erencan Uysal <56518226+erencanuysal@users.noreply.github.com>
2021-05-30 21:21:47 +0300

    Update test_comment.py

--- practice-app/test/test_comment.py
+++ practice-app/test/test_comment.py
@@ -12,14 +12,14 @@ comments = mydb["comments"]
 class TestStringMethods(unittest.TestCase):
 
     def test_of_comment_post(self):
-        rsp = requests.post('http://127.0.0.1:5000/post/1/comments/new/erencan', {'text': 'I love this story'})
+        rsp = requests.post('http://127.0.0.1:5000/api/post/1/comments/new/erencan', {'text': 'I love this story'})
         print(len(list(comments.find({'postId': 1}, {'_id': False}))))
         self.assertEqual(len(list(comments.find({'postId': 1}, {'_id': False}))), 1)
-        rsp = requests.post('http://127.0.0.1:5000/post/1/comments/new/atainan', {'text': 'I don\'t like this story'})
+        rsp = requests.post('http://127.0.0.1:5000/api/post/1/comments/new/atainan', {'text': 'I don\'t like this story'})
         self.assertEqual(len(list(comments.find({'postId': 1}, {'_id': False}))), 2)
         comments.delete_many({'postId':1})
     def test_of_tpapi(self):
-        rsp = requests.post('http://127.0.0.1:5000/post/1/comments/new/erencan', {'text': 'Benim adım ali. 13 yaşındayım.'})
+        rsp = requests.post('http://127.0.0.1:5000/api/post/1/comments/new/erencan', {'text': 'Benim adım ali. 13 yaşındayım.'})
         self.assertEqual(rsp.json()["language"],"tr")
         comments.delete_many({'postId': 1})
 
