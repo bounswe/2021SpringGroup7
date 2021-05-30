9ba388838a053542836079ca7984800a9ffde351
efesabanogluu <56563527+efesabanogluu@users.noreply.github.com>
2021-05-30 22:52:46 +0300

    Update location.py

--- practice-app/location/location.py
+++ practice-app/location/location.py
@@ -12,26 +12,30 @@ def createLocation(post,username):
     locationDetails=request.get_json()
     address = locationDetails['address']
     key = "AIzaSyBUmonTQ91F6jFXZij-JgUmF2t8l8XT0Es"
+    try:
+        coordinates =requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key)
+        latitude = coordinates['geometry']['location']['lat']
+        longitude = coordinates['geometry']['location']['lng']
+
+        relatedLocations = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=' + key + '&location=' + latitude + "," + longitude + '&radius=10000')
+        # db.locations.find_one({})
+        # postToBeEdited = db.posts.find_one({'_id': ObjectId(postId)})
+
+        if coordinates['status'] != "OK":
+            return "Bir hata var!"
+
+        location = {
+            "locationId": (latitude+longitude),
+            "address": address,
+            "latitude": latitude,
+            "longitude":longitude,
+            "date": datetime.datetime.now(),
+            "relatedLocations":relatedLocations
+        }
+
+        db.locations.insert_one(location)
+        return jsonify(location)
+        return(jsonify({'status': 200, location}))
+    except :
+        abort(jsonify(404,'create-location-error' ))
     
-    coordinates =requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key)
-    latitude = coordinates['geometry']['location']['lat']
-    longitude = coordinates['geometry']['location']['lng']
-
-    relatedLocations = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=' + key + '&location=' + latitude + "," + longitude + '&radius=10000')
-    # db.locations.find_one({})
-    # postToBeEdited = db.posts.find_one({'_id': ObjectId(postId)})
-
-    if coordinates['status'] != "OK":
-        return "Bir hata var!"
-
-    location = {
-        "locationId": (latitude+longitude),
-        "address": address,
-        "latitude": latitude,
-        "longitude":longitude,
-        "date": datetime.datetime.now(),
-        "relatedLocations":relatedLocations
-    }
-
-    db.locations.insert_one(location)
-    return jsonify(location)
