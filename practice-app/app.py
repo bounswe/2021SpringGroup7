6bc58a64f9650e2afc05af8afac8ed1ffb1c5e9a
mrabiabrn <merverabiabarin@gmail.com>
2021-06-01 16:46:35 +0300

    deleted redundant post inserts

--- practice-app/app.py
+++ practice-app/app.py
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
-from flask import Flask, jsonify, request
-=======
-from flask import Flask, jsonify, make_response
->>>>>>> ab79b9339d580d82038955e489a60f4662fa72dd
+from flask import Flask, jsonify, request, make_response
 from .database import mongo
 from .profile import profile
 from .viewPost import viewPost
 from .editPost import editPost
 
-<<<<<<< HEAD
-from flask import make_response
 
-import datetime #
-=======
 import datetime
 
 from .likes import likes
->>>>>>> ab79b9339d580d82038955e489a60f4662fa72dd
+
 
 app = Flask(__name__)
 
@@ -106,34 +98,6 @@ db.likes.insert_one({
     "date": datetime.datetime.now(),
 })
 
-db.posts.insert_one({
-        'id'        : '1',
-        'owner_username'     : 'atainan',
-        'topic'     : 'Great Day In Rome...',
-        'story'     : 'I was in Rome for about 3 months...',
-        'location'  : 'Rome',
-        'postDate'  : datetime.datetime(2021, 5, 27, 12, 59, 40, 2), 
-        'storyDate' : {'start': datetime.datetime(2017, 1, 1), 'end': datetime.datetime(2017, 3, 1)}, 
-        'multimedia': ['photo_link_1','photo_link_2'],
-        'tags'      : ['summer', 'musical', 'day'],
-        'userComments'  : [{'username': 'ryan', 'comment': 'great memory!'}],
-        'lastEdit'      : ' ' 
-
-})
-
-db.posts.insert_one({
-        'id'        : '2',
-        'owner_username'     : 'ryan',
-        'topic'     : 'Notre Dame de Paris Fire...',
-        'story'     : 'There was a fire...',
-        'location'  : 'Notre-Dame de Paris',
-        'postDate'  : datetime.datetime(2021, 5, 29, 22, 30, 45, 20), 
-        'storyDate' : {'start': datetime.datetime(2019, 4, 15), 'end': datetime.datetime(2019, 4, 15)}, 
-        'multimedia': ['photo_link_1','photo_link_2'],
-        'tags'      : ['fire', 'damage', 'history'],
-        'userComments'  : [{'username': 'ryan', 'comment': 'it is so sad'}],
-        'lastEdit'      : ' ' 
-})
 
 
 @app.route('/', methods=['GET'])
