cf9b9c18b3e7cd35e220c512fd04cbcdbbc06efa
mrabiabrn <merverabiabarin@gmail.com>
2021-06-01 13:59:21 +0300

    fixed status codes

--- practice-app/test/test_viewPost.py
+++ practice-app/test/test_viewPost.py
@@ -7,35 +7,33 @@ import json
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["db"]
 posts = mydb["posts"]
-attributes = set(["_id", "owner", "lastEdit", "location", "multimedia","postDate", 
-                                    "similarTags", 
-                                    "story", 
-                                    "storyDate", 
-                                    "tags", 
-                                    "topic", 
-                                    "userComments"])
+attributes = set([  "id", "owner_username", 
+                    "lastEdit", "location", 
+                    "multimedia","postDate", 
+                    "similarTags",  "story", 
+                    "storyDate", "tags", 
+                    "topic", "userComments"])
 
 class TestPostDetail(unittest.TestCase):
 
     def test_empty_endpoint(self):
-        response = requests.get("http://127.0.0.1:5000/api/postDetail/")
+        response = requests.get("http://127.0.0.1:5000/api/viewPost/")
         self.assertEqual(response.status_code, 404)
 
-    def test_db_post_existence(self):
-         self.assertGreaterEqual(len(list(posts.find())), 1)
+    def test_post_not_found(self):
+        response = requests.get("http://127.0.0.1:5000/api/viewPost/-1")
+        self.assertEqual(response.status_code, 404)
 
-    def test_post_notfound(self):
-        postId = posts.find_one({},{"_id":1})
-        posts.delete_one({'_id' : postId})
-        response = requests.get(f"http://127.0.0.1:5000/api/postDetail/{postId}")
-        self.assertEqual(response.status_code, 500)
-    
-    def test_json_request(self):
-        postId = str(posts.find_one({},{"_id":1})['_id'])
-        response = requests.get(f"http://127.0.0.1:5000/api/postDetail/{postId}")
-        self.assertEqual(response.status_code, 200)
+    def test_api_call(self):
+        response = requests.get("http://127.0.0.1:5000/api/viewPost/1")
         response_json = json.loads(response.text)
-        self.assertEqual(set(response_json.keys()),attributes)              # make sure it returns right attributes
+        self.assertEqual('similarTags' in response_json.keys(), True)
+
+    def test_view_post(self):
+        response = requests.get("http://127.0.0.1:5000/api/viewPost/1")
+        response_json = json.loads(response.text)
+        self.assertEqual(set(response_json.keys()),attributes)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
