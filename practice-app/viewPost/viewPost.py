b65026ad6eed37d920676c9a5cddcad02505dea6
mrabiabrn <merverabiabarin@gmail.com>
2021-06-01 13:47:06 +0300

    fixed status codes

--- practice-app/viewPost/viewPost.py
+++ practice-app/viewPost/viewPost.py
@@ -1,43 +1,41 @@
 from flask import Blueprint, jsonify, request, abort
-#from werkzeug.wrappers import request
 from ..database import mongo
-from bson.objectid import ObjectId
 
 import http.client
 import json
-import os
-
 
 viewPostDetails_bp = Blueprint('View Post Details', __name__)
 
-@viewPostDetails_bp.route('/api/postDetail/<string:postId>', methods=['GET'])
+@viewPostDetails_bp.route('/api/viewPost/<string:postId>', methods=['GET'])
 def viewPost(postId):
     
     db = mongo.db
     postToBeViewed = db.posts.find_one({'id': postId}, {'_id': False})     # find post with the given id 
 
     if not postToBeViewed:  
-        abort(500, "Invalid post id")                                           
+        abort(404)                                           
 
-    '''
-        https://rapidapi.com/wordgrabbag/api/similar-words    
-        This api is used for getting the words similar to tags.                      
-    '''
+    similarTags = callSimilarTags(postToBeViewed)
+    postToBeViewed['similarTags'] = similarTags     
+
+    return jsonify(postToBeViewed)
+
+
+'''
+    https://rapidapi.com/wordgrabbag/api/similar-words    
+    This 3rd party API is used for getting similar words to tags of the story post.                      
+'''
+def callSimilarTags(postToBeViewed):
+    
     conn = http.client.HTTPSConnection("similarwords.p.rapidapi.com")
-    
-    # get api key
-    with open(os.path.dirname(__file__) + '/../.apiKey') as f:
-        apiKey = f.read()
-    
     headers = {
-        'x-rapidapi-key': apiKey,
+        'x-rapidapi-key': "eedd55b42fmshad927cbdcb4feafp1fe3fejsnfb92b2735386",
         'x-rapidapi-host': "similarwords.p.rapidapi.com"
         }
 
     similarTags = []
-    for tag in postToBeViewed['tags']:      # get list of similar words from 3rd party api for each tag of the post
+    for tag in postToBeViewed['tags']:      
         
-        # make a call to get list of similars of a word
         conn.request("GET", "/moar?query=" + tag, headers=headers)
         res = conn.getresponse()
         data = res.read()
@@ -46,11 +44,6 @@ def viewPost(postId):
 
         similarList = []
         if 'result' in dictData:                  
-            similarList = dictData['result']        # similar list for a tag
+            similarList = dictData['result']        # similars of a tag
 
         similarTags.extend(similarList)
-    
-    postToBeViewed['similarTags'] = similarTags     # add similar tags to result
-
-    return jsonify(postToBeViewed)
-
