70145df94a23a53641e28771c1a9416f6339b227
Abdulkadir Elmacı <abdulkadirelmaci@gmail.com>
2021-06-04 21:36:03 +0300

    Added sentry support for tracking errors

--- practice-app/app.py
+++ practice-app/app.py
@@ -17,6 +17,24 @@ from api.editPost import editPost
 from api.likes import likes
 from api.search import search
 import logging
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://0cde92986781428cbdf66f7cdc55f2df@o793703.ingest.sentry.io/5801382",
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
 
 app = Flask(__name__)
 
