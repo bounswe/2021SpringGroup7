a4dcf716ffdddecfa78b85ccfc2e88518b0dbe89
kocasariumut <umut.denizli@hotmail.com>
2021-06-06 14:33:38 +0300

    documentation added for reportedUser functionality and refactoring

--- practice-app/api/report/reportedUser.py
+++ practice-app/api/report/reportedUser.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify
 from database import mongo
+from flasgger import swag_from
 
 report_bp = Blueprint('Report', __name__)
 
 @report_bp.route('/api/report/<int:userId>', methods=['POST'])
+@swag_from('../../apiDocs/report/reportedUser.yml')
 def postReport(userId):
 
     db = mongo.db
