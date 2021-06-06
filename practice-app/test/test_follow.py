ed1e1600ce6b1b3ea235079dc4a103441ff08736
Abdulkadir Elmacı <abdulkadirelmaci@gmail.com>
2021-06-06 14:19:01 +0300

    Added ids to the dummyvars. Renamed the mock variables to fit to the convention

--- practice-app/test/test_follow.py
+++ practice-app/test/test_follow.py
@@ -12,26 +12,29 @@ class FollowTestCases(unittest.TestCase):
 
     @patch('api.follow.follow.getUserFromDb')
     @patch('api.follow.follow.addToUserArrayInDb')
-    def test_user_follow(self, mockGet, mockAdd):
-        dummyVars = [{'username': 'ryan','isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': []},
-                     {'username': 'ata', 'isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': []}]
-        mockGet.side_effect = dummyVars
+    def test_user_follow(self, mock_addToUserArrayInDb, mock_getUserFromDb):
+        dummyVars = [{"_id": 12, 'username': 'ryan','isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': []},
+                     {"_id": 13, 'username': 'ata', 'isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': []}]
+        mock_getUserFromDb.side_effect = dummyVars
+
         response = followUser('ryan', 'ata')
         expected = "Success", 200
         self.assertEqual(response, expected)
 
     @patch('api.follow.follow.getUserFromDb')
-    def test_user_is_already_followed(self, mockGet):
-        dummyVars = [{'username': 'ryan', 'isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': ['ata']},
-                     {'username': 'ata', 'isVisible': 'True', 'followRequests': [], 'followers': ['ryan'], 'followings': []}]
-        mockGet.side_effect = dummyVars
+    def test_user_is_already_followed(self, mock_getUserFromDb):
+        dummyVars = [{"_id": 12, 'username': 'ryan', 'isVisible': 'True', 'followRequests': [], 'followers': [], 'followings': ['ata']},
+                     {"_id": 13, 'username': 'ata', 'isVisible': 'True', 'followRequests': [], 'followers': ['ryan'], 'followings': []}]
+        mock_getUserFromDb.side_effect = dummyVars
+
         resp = followUser('ryan', 'ata')
         expected = "Already followed", 200
         self.assertEqual(resp, expected)
 
     @patch('api.follow.follow.getUserFromDb')
-    def test_user_none(self, mockGet):
-        mockGet.side_effect = [None, None]
+    def test_user_none(self, mock_getUserFromDb):
+        mock_getUserFromDb.side_effect = [None, None]
+
         with self.assertRaises(HTTPException):
             followUser("notexisting", "notexisting")
 
