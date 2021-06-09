ccb0cc5d13f1fe0c32d99aa0b83551316d6a28f2
Erencan Uysal <uysalcmpe@gmail.com>
2021-06-09 22:02:56 +0300

    add comments to comment.py

--- practice-app/backend/api/comment/comment.py
+++ practice-app/backend/api/comment/comment.py
@@ -6,7 +6,7 @@ from flasgger import swag_from
 
 
 comment_bp = Blueprint('Comments', __name__)
-
+#This endpoint returns comments of posts from database
 @comment_bp.route('/api/post/<int:post>/comments', methods=['GET'])
 @swag_from('../../apidocs/comment/getComments.yml')
 def getComments(post):
@@ -20,6 +20,7 @@ def getComments(post):
         abort(404, 'get comment error:no comments in this post')
 
     return jsonify(dbresponse)
+#This endpoint takes a string from user and create a comment
 @comment_bp.route('/api/post/<int:post>/comments/new/<string:username>', methods=['POST'])
 @swag_from('../../apidocs/comment/makeComment.yml')
 def makeComment(post,username):
@@ -34,10 +35,12 @@ def makeComment(post,username):
 
     commentText=request.form
     text = commentText.get('text')
+
+    #I used detectlanguage as third party api. It returns possible language of text.
     lang=requests.post('https://ws.detectlanguage.com/0.2/detect',{'q':text},auth=('a133a45152facfa243ff514da79b9a09','')).json()
     language=lang['data']['detections'][0]['language']
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_string = now.strftime("%d/%m/%Y %H:%M")          #Date of comment day/month/year hour:minute
     commentj = {
         "username": username,
         "comment": text,
@@ -53,7 +56,7 @@ def makeComment(post,username):
             'language':language
     })
     count = postinfo['numberOfComments']
-    newnumber = {'$set': {'numberOfComments': count + 1}}
+    newnumber = {'$set': {'numberOfComments': count + 1}}           #increment comment number in post object
     db.posts.update_one(postinfo,newnumber)
     return jsonify(commentj)
 
