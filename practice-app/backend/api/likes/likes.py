50066dbeb894181cdc3a27c9af1f1ae875724912
onurcanavci <onur.cann.avci@gmail.com>
2021-06-10 01:21:03 +0300

    Add Swagger Integrations to Likes api and Fix get likes api

--- practice-app/backend/api/likes/likes.py
+++ practice-app/backend/api/likes/likes.py
@@ -1,14 +1,16 @@
 from flask import Blueprint, jsonify, abort
 from database import mongo
+from flasgger import swag_from
 import datetime
 
 likes_bp = Blueprint('Like action and viewing user which likes post', __name__)
 
 @likes_bp.route('/api/post/<int:postId>/likes', methods=['GET'])
+@swag_from('../../apidocs/likes/getLikes.yml')
 def getLikes(postId):
     postExist = findPostFromDb(postId)
     userThatLikesPost = getLikesFromDb(postId)
-    numberOfPostLike = list(userThatLikesPost)
+    postLikes = list(userThatLikesPost)
 
     if not postExist:
         abort(404, 'Post is not found')
@@ -17,7 +19,7 @@ def getLikes(postId):
         abort(404, 'Post is not liked')
 
     data = {
-        'items': list(userThatLikesPost),
+        'items': postLikes,
         'totalCount': userThatLikesPost.count()
     }
     
@@ -26,6 +28,7 @@ def getLikes(postId):
     return jsonify(data)
 
 @likes_bp.route('/api/post/<int:postId>/likes/<string:username>', methods=['POST'])
+@swag_from('../../apidocs/likes/likePost.yml')
 def likePost(postId,username):
     postExist = findPostFromDb(postId)
     userExist = findUserFromDb(username)
