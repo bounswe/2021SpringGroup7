509825325c88c6fac3e8fc6c4fe2b989898e10b7
Umut Kocasarı <umutk@armut.com>
2021-11-09 16:57:42 +0300

    all cors domains are added

--- Columbus/backend/src/settings.py
+++ Columbus/backend/src/settings.py
@@ -51,9 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'src.urls'
 
