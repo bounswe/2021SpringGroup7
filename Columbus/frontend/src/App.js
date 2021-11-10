24674693a164d5f48af7a3d3401785ed886d16db
mrabiabrn <merverabiabarin@gmail.com>
2021-11-10 21:10:51 +0300

    added environment API_BASE

--- Columbus/frontend/src/App.js
+++ Columbus/frontend/src/App.js
@@ -10,6 +10,12 @@ import React, { useState, useEffect } from 'react';
 //         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
 // }
 
+var API_BASE="http://ec2-35-158-103-6.eu-central-1.compute.amazonaws.com:8000/test/hello/";
+
+if (process.env.NODE_ENV==="development") {
+  API_BASE="http://localhost:8000/test/hello/";
+}
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
   const [item, setItem] = useState([])
@@ -17,7 +23,7 @@ function App() {
   useEffect(() => {
 
     let message = '! FRONT TEAM !'
-    fetch("http://localhost:8000/test/hello/" + message)
+    fetch(API_BASE + message)
       .then((result) => result.json())
       .then(result => {
         setIsLoaded(true)
