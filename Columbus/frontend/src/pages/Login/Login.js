d0fcdf684cbff6309537e3450e7bbaba5ea486f9
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2021-11-14 22:01:32 +0300

    Add logo and register

--- Columbus/frontend/src/pages/Login/Login.js
+++ Columbus/frontend/src/pages/Login/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core';
-import logo from '../../assets/Columbus.svg'
+import Logo from '../../components/Logos/LogoWithText'
 import LoginForm from '../../components/Forms/LoginForm/LoginForm';
+import ModalDialog from '../Register/ModalDialog'
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
     Applogo: {
@@ -12,22 +14,45 @@ const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'end',
-        alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& .MuiTextField-root': {
+			margin: theme.spacing(1),
+			width: '300px',
+		},
+		'& .MuiButtonBase-root': {
+			margin: theme.spacing(2),
+		},
     },
 }));
 
-export default function Login(){
+export default function Login({setAuthenticated}){
     const classes= useStyles();
-    const handleClose = (email, password) => {
+    
+    const [openRegister, setOpenRegister] = useState(false);
+    
+    const handleOpenRegister = () => {
+        setOpenRegister(true)
+    };
+    
+    const handleCloseRegister = () => {
+    setOpenRegister(false);
+    };
+
+    const handleLogin = (email, password) => {
         console.log(email, password);
         localStorage.setItem('jwtToken', 'bearer ' + password);
+        setAuthenticated(true);
     };
 
     return (
         <div className={classes.root}>
-            <img src={logo} className = {classes.Applogo} alt='Logo' />
-            <LoginForm handleClose={handleClose} />
+            <Logo className={classes.Applogo} alt='Logo'/>
+            <LoginForm handleClose={handleLogin} />
+            <Button variant="contained" color="primary" onClick={handleOpenRegister}>
+					        Register
+            </Button>
+            <ModalDialog open={openRegister} handleClose={handleCloseRegister} />  
         </div>
     )
 }
