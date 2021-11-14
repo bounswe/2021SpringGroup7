18e641f46cef5feb036894fa5680c49e47b13614
efesabanoglu <efe.sabanoglu@fireflyon.com>
2021-11-14 16:19:12 +0300

    Login API is ready.

--- Columbus/backend/guest/views.py
+++ Columbus/backend/guest/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect,JsonResponse
 from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from user.models import User
 
 
@@ -21,3 +21,30 @@ def register(request):
         return JsonResponse({'return': '{} is succesfully created'.format(user.user_name)})
     else:
         return JsonResponse({'return':'Send a Post request'})
+
+
+def login(request):
+    if request.method == 'POST':
+        user_name = request.GET.get('user_name')
+        password = request.GET.get('password')
+        # user = auth.authenticate(username=username, password=password)
+        # user = authenticate(username=username, password=password)
+        #
+        # if user is not None:
+        #     return JsonResponse({'return': '{} is succesfully login'.format(user.user_name)})
+        #     # auth.login(request, user)
+        #     # return redirect('/home')
+        # else:
+        #     return JsonResponse("there is a error.")
+        #     # return redirect("/")
+        try:
+            user = User.objects.get(user_name=user_name)
+            encoded_pw = user.password
+            if check_password(password, encoded_pw):
+                return JsonResponse({'return': '{} is succesfully login'.format(user.user_name)})
+            else:
+                return JsonResponse({'return': 'password is incorrect'})
+        except:
+            return JsonResponse({'return': '{} is not existing username'.format(user_name)})
+    else:
+        return JsonResponse({'return': 'Send a Post request'})
