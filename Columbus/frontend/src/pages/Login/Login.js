01d760ce9a3eb7c1dfae1f9a07281de11ea67b0d
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2021-11-14 22:31:49 +0300

    Add backend login request

--- Columbus/frontend/src/pages/Login/Login.js
+++ Columbus/frontend/src/pages/Login/Login.js
@@ -23,13 +23,20 @@ const useStyles = makeStyles(theme => ({
 		'& .MuiButtonBase-root': {
 			margin: theme.spacing(2),
 		},
-    },
+    }
 }));
+
+var API_BASE = "http://ec2-35-158-103-6.eu-central-1.compute.amazonaws.com:8000";
+
+if (process.env.NODE_ENV === "development") {
+  API_BASE = "http://localhost:8000";
+}
 
 export default function Login({setAuthenticated}){
     const classes= useStyles();
     
     const [openRegister, setOpenRegister] = useState(false);
+    const [isError, setError] = useState(false);
     
     const handleOpenRegister = () => {
         setOpenRegister(true)
@@ -39,16 +46,36 @@ export default function Login({setAuthenticated}){
     setOpenRegister(false);
     };
 
-    const handleLogin = (email, password) => {
-        console.log(email, password);
-        localStorage.setItem('jwtToken', 'bearer ' + password);
-        setAuthenticated(true);
+    const handleLogin = (username, password) => {
+        console.log(username, password);
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({'user_name': username,  password})
+        };
+        fetch(API_BASE + '/guest/login', requestOptions)
+        .then(response => {
+            if(response.ok){
+                response.json()
+            }
+            throw new Error(response.json()['return'])
+        })
+        .then(data => {
+            setAuthenticated(true)
+            localStorage.setItem('jwtToken', 'bearer ' + password);
+        })
+        .catch((reason) => {
+            setError(true);
+            console.error(reason);
+        });
+    
     };
+    
 
     return (
         <div className={classes.root}>
             <Logo className={classes.Applogo} alt='Logo'/>
-            <LoginForm handleClose={handleLogin} />
+            <LoginForm handleClose={handleLogin} showError={isError} />
             <Button variant="contained" color="primary" onClick={handleOpenRegister}>
 					        Register
             </Button>
