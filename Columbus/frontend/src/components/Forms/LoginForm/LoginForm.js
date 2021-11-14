44680b5a5af7a62b52dd6bb54a4a6ab6f39249ee
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2021-11-14 14:31:23 +0300

    Add login form component

---
+++ Columbus/frontend/src/components/Forms/LoginForm/LoginForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '300px',
+        },
+        '& .MuiButtonBase-root': {
+            margin: theme.spacing(2),
+        },
+    },
+}));
+
+const LoginForm = ({ handleClose }) => {
+    const classes = useStyles();
+ 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        handleClose(email, password);
+    };
+
+    return ( 
+    < form 
+    className = { classes.root }
+    onSubmit = { handleSubmit } 
+    >
+        <TextField 
+        label = "Email"
+        variant = "filled"
+        type = "email"
+        required value = { email }
+        onChange = { e => setEmail(e.target.value) }
+        /> 
+        
+        <TextField 
+        label = "Password"
+        variant = "filled"
+        type = "password"
+        required value = { password }
+        onChange = { e => setPassword(e.target.value) }
+        /> 
+        <div>
+        
+        <Button 
+        type = "submit"
+        variant = "contained"
+        color = "primary" 
+        >
+            Login 
+        </Button> 
+        </div> 
+    </form>
+    );
+};
+
+export default LoginForm;
