e2d0d18bec975d12be45f1757b3f73aa771d3c77
erencanuysal <uysalcmpe@gmail.com>
2021-11-15 10:31:45 +0300

    adding backend connection

--- Columbus/frontend/src/pages/Register/ModalDialog.js
+++ Columbus/frontend/src/pages/Register/ModalDialog.js
@@ -6,8 +6,7 @@ const ModalDialog = ({ open, handleClose }) => {
 	return (
 		// props received from App.js
 		<Dialog open={open} onClose={handleClose}>
-			// form to be created
-			<Form handleClose={handleClose} />{' '}
+			<Form handleClose={handleClose} />
 		</Dialog>
 	);
 };
--- Columbus/frontend/src/pages/Register/Register.js
+++ Columbus/frontend/src/pages/Register/Register.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
 import { makeStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+var API_BASE = 'http://ec2-35-158-103-6.eu-central-1.compute.amazonaws.com:8000/';
+if (process.env.NODE_ENV === 'development') {
+	API_BASE = 'http://localhost:8000/';
+}
 const useStyles = makeStyles((theme) => ({
 	root: {
 		display: 'flex',
@@ -25,34 +29,57 @@ const Form = ({ handleClose }) => {
 	const classes = useStyles();
 	// create state variables for each input
 	const [ firstName, setFirstName ] = useState('');
+	const [ userName, setUserName ] = useState('');
 	const [ lastName, setLastName ] = useState('');
 	const [ email, setEmail ] = useState('');
 	const [ password, setPassword ] = useState('');
-
+	const [ message, setMessage] = useState('');
+	const [ error, setError] = useState(false);
+	const [ isLoaded, setIsLoading] = useState(false);
 	const handleSubmit = (e) => {
+		setIsLoading(true);
 		e.preventDefault();
-		console.log(firstName, lastName, email, password);
-		handleClose();
+		const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({'user_name': userName,  'first_name':firstName,'last_name':lastName,'user_email':email,'password':password})
+        };
+        fetch(API_BASE + '/guest/register', requestOptions)
+        .then(response => {
+			setMessage({items : response.response})
+			handleClose();
+		}
+		)
+        .catch((error) => {setError(true)
+			
+        }).finally(()=>{
+			setIsLoading(false)});;
+		
 	};
 
 	return (
-		<form className={classes.root} onSubmit={handleSubmit}>
+		<form className={classes.root} onSubmit={handleSubmit} showError={message} >
+			<TextField
+				label="User Name"
+				variant="filled"
+				required
+				value={userName}
+				onChange={(e) => setUserName(e.target.value)}
+			/>
 			<TextField
 				label="First Name"
 				variant="filled"
 				required
 				value={firstName}
 				onChange={(e) => setFirstName(e.target.value)}
-			/>{' '}
-			{' '}
+			/>
 			<TextField
 				label="Last Name"
 				variant="filled"
 				required
 				value={lastName}
 				onChange={(e) => setLastName(e.target.value)}
-			/>{' '}
-			{' '}
+			/>
 			<TextField
 				label="Email"
 				variant="filled"
@@ -60,8 +87,7 @@ const Form = ({ handleClose }) => {
 				required
 				value={email}
 				onChange={(e) => setEmail(e.target.value)}
-			/>{' '}
-			{' '}
+			/>
 			<TextField
 				label="Password"
 				variant="filled"
@@ -69,19 +95,17 @@ const Form = ({ handleClose }) => {
 				required
 				value={password}
 				onChange={(e) => setPassword(e.target.value)}
-			/>{' '}
-			{' '}
+			/>
 			<div>
 				<Button variant="contained" onClick={handleClose}>
-					Cancel {' '}
-				</Button>{' '}
-				{' '}
+					Cancel 
+				</Button>
 				<Button type="submit" variant="contained" color="primary">
-					Signup {' '}
-				</Button>{' '}
-				{' '}
-			</div>{' '}
-			{' '}
+					Signup 
+				</Button>
+			</div>
+			{isLoaded ? <p >Loading...</p> : null}
+			{error ? <p className={classes.error}>Unsuccessfull register!</p> : null}
 		</form>
 	);
 };
