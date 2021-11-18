c249ce95d40ee52d844b4ceacd1d06f19021a76e
onurcan.avci <onur.cann.avci@gmail.com>
2021-11-19 00:15:10 +0300

    CM-3 Fix import error

--- Columbus/frontend/src/pages/Register/Register.js
+++ Columbus/frontend/src/pages/Register/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import MessageDialog from "../../components/Dialogs/MessageDialog";
+import MessageDialog from "../../components/Dialogs/MessageDialog/MessageDialog";
 import { makeStyles } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
