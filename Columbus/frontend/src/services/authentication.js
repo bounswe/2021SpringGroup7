c2ff33ae2b3399fd9a49cc7582cf4a9f0601807f
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2021-11-28 22:52:13 +0300

    Create authentication API call service

---
+++ Columbus/frontend/src/services/authentication.js
@@ -0,0 +1,9 @@
+import { API_INSTANCE } from "../config/api";
+
+const AUTHENTICATION_SERVICE = {
+  LOG_IN: (username, password) => API_INSTANCE.post("/guest/login/", {user_name: username, password}),
+  LOG_OUT: (userId) => API_INSTANCE.post(`/users/${userId}/logout/`),
+  REGISTER: (data) => API_INSTANCE.post("/guest/register/", data),
+};
+
+export default AUTHENTICATION_SERVICE;
