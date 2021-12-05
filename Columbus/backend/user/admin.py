a724645f9291e7e6854c68a472333127bd1b0705
kocasariumut <umut.denizli@hotmail.com>
2021-12-05 17:22:51 +0300

    database models added

--- Columbus/backend/user/admin.py
+++ Columbus/backend/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Story)
+admin.site.register(Tag)
+admin.site.register(Location)
+admin.site.register(Report)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Following)
+admin.site.register(Profile)
