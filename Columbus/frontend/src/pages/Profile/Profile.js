613733af7648b9445825c33df71a32a9dbf3e203
mrabiabrn <merverabiabarin@gmail.com>
2021-12-06 02:23:43 +0300

    initialize profile pagelayout

--- Columbus/frontend/src/pages/Profile/Profile.js
+++ Columbus/frontend/src/pages/Profile/Profile.js
@@ -4,16 +4,31 @@ import {
   Paper,
   Avatar,
   Container,
+  Grid,
   Box,
   CircularProgress,
   Typography,
   DialogTitle,
   Dialog,
   makeStyles,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Divider
 } from "@material-ui/core";
-import PostCard from "../../components/Post";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CakeIcon from '@mui/icons-material/Cake';
+import Post from "../../components/Post/Post";
 import api from "../../services/post";
 import CreatePostDialog from "../../components/CreatePost/index";
+import {dummyPosts} from "../Home/Home.constants"
+import { shadows } from '@mui/system';
+import Stack from '@mui/material/Stack';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 const useStyles = makeStyles((theme) => ({
   toolbar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
@@ -42,19 +57,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 function Profile(props) {
   const classes = useStyles();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
-  const [name, setName] = useState("");
-  const [nofFollowers, setNofFollowers] = useState(0);
-  const [nofFollowing, setNofFollowing] = useState(0);
+  const [name, setName] = useState("Salih Yilmaz");
+  const [nofFollowers, setNofFollowers] = useState(3);
+  const [nofFollowing, setNofFollowing] = useState(3);
   const [OpenDialogFollowing, setOpenDialogFollowing] = React.useState(false);
   const [OpenDialogFollower, setOpenDialogFollower] = React.useState(false);
   const [username, setUsername] = useState("");
   const [following, setFollowing] = useState([]);
   const [follower, setFollower] = useState([]);
   const [postIds, setPostIds] = useState([]);
+  
+  const [value, setValue] = React.useState('one');
+
 
   const handleClickOpenDialogFollowing = () => {
     setOpenDialogFollowing(true);
@@ -70,29 +89,20 @@ function Profile(props) {
     setOpenDialogFollower(false);
   };
 
-  useEffect(() => {
-    api
-      .GET_PROFILE("atainan")
-      .then((resp) => {
-        setLoading(false);
-        setUsername(resp.username);
-        setPosts(resp.posts);
-        setName(resp.first_name + " " + resp.last_name);
-        setNofFollowers(resp.nofFollowers);
-        setNofFollowing(resp.nofFollowings);
-        setFollowing(resp.followings);
-        setFollower(resp.followers);
-        setPostIds(resp.postIds);
-      })
-      .catch((error) => alert(error));
-  }, []);
-
   const renderEmptyPost = () => {
     return (
       <Box className={classes.emptyPost}>
         <Typography>You do not shared story yet!</Typography>
       </Box>
     );
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
   };
 
   if (loading) {
@@ -108,150 +118,217 @@ function Profile(props) {
     <div>
       <Wrapper>
         <Paper
-          elevation={3}
+          elevation={1}
           style={{
-            width: "100%",
+            width: "98%",
             minHeight: 1090,
             padding: "4%",
-            borderRadius: 10,
-            marginBottom: 40,
+            margin: "1%",
+            borderRadius: 0,
           }}
         >
-          <Container style={{ display: "flex", flexDirection: "row" }}>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.internethaber.com/storage/files/images/2019/05/08/avatar-2-3-ve-4un-vizyon-tarihle-lna9_cover.jpg"
-              elevation={10}
-              style={{
-                width: 100,
-                height: 100,
-                marginLeft: 50,
-              }}
-            />
+       
 
-            <div>
-              <Typography
-                component="h2"
-                variant="h3"
-                color="inherit"
-                align="center"
-                noWrap
-                className={classes.toolbarTitle}
-                gutterBottom
-              >
-                {name}
-              </Typography>
+          <Container fixed>
+            <Box sx={{ height: '50vh', boxShadow: 3  }}>
 
-              <Container style={{ display: "flex", flexDirection: "row" }}>
-                <p
-                  onClick={handleClickOpenDialogFollowing}
-                  style={{ margin: 20, fontSize: 30 }}
-                >
-                  Following: {nofFollowing}
-                </p>
-                <Dialog
-                  open={OpenDialogFollowing}
-                  onClose={handleCloseDialogFollowing}
-                  style={{ minWidth: 300, borderRadius: 10 }}
-                  aria-labelledby="simple-dialog-title"
-                >
-                  <DialogTitle
-                    id="simple-dialog-title"
-                    style={{ minWidth: 300, textAlign: "center" }}
-                  >
-                    Followings
-                  </DialogTitle>
-                  {following.map((item, index) => {
-                    return (
-                      <Container
-                        key={index}
-                        style={{
-                          display: "flex",
-                          flexDirection: "row",
-                          padding: "4%",
-                          textAlign: "center",
-                        }}
+              <Grid xs container direction="column" spacing={6}>
+
+                <Grid item container spacing={5}>
+
+                  <Grid item container direction="column" xs={2} spacing={2}>
+                    
+                    <Grid item xs >
+                      <Button sx={{ height: '100', width: '100' }}>
+                        <Avatar >S</Avatar>
+                      </Button>
+                    </Grid>
+
+                    <Grid item xs>
+                         <Stack>
+                          <Button 
+                            size="medium"
+                            variant="text"
+                            startIcon={ <LocationOnIcon color="primary"></LocationOnIcon>}
+                            style={{textTransform: 'none'}} 
+                            >
+                              Ankara
+                            </Button>
+                          <Button 
+                            size="medium"
+                            variant="text"
+                            startIcon={ <CakeIcon color="primary"></CakeIcon>}
+                            >
+                              01.01.1960
+                            </Button>
+                          </Stack>
+                    </Grid>
+
+                    <Grid item xs>
+                          <Button 
+                            color="primary" 
+                            variant="contained"
+                            style={{textTransform: 'none'}} 
+                            >
+                              Edit Profile
+                          </Button>
+                    </Grid>
+
+                  </Grid>
+
+                  <Grid item xs={8} sm>
+
+                        <Typography gutterBottom variant="h5" component="div">
+                          Salih Yılmaz
+                        </Typography>
+                        <Typography variant="h7" gutterBottom>
+                          salihyilmaz
+                        </Typography>
+                        <Typography variant="h6" color="primary">
+                          About Me
+                        </Typography>
+                        <Typography variant="body2">
+                          I am a retired railroad officer. It was always a pleasure for me to share my memories with other people. Here, you can find many of them!.
+                          <br />
+                          {'- An Old Story Teller'}
+                        </Typography>
+
+                  </Grid>
+
+                  <Grid item xs={2}>
+                    <Stack>
+                      <Button
+                        onClick={handleClickOpenDialogFollowing}
                       >
-                        <Avatar
-                          alt="r b"
-                          src=""
-                          elevation={10}
-                          style={{
-                            width: 40,
-                            height: 40,
-
-                            marginRight: "4%",
-                          }}
-                        >
-                          {item.substring(0, 2).toUpperCase()}
-                        </Avatar>
-                        <h3 style={{ margin: 0, marginTop: "3%" }}>{item}</h3>
-                      </Container>
-                    );
-                  })}
-                  {/* {getFollowing()} */}
-                </Dialog>
-                <p
-                  style={{ margin: 20, fontSize: 30 }}
-                  onClick={handleClickOpenDialogFollower}
-                >
-                  Followers: {nofFollowers}
-                </p>
-                <Dialog
-                  open={OpenDialogFollower}
-                  onClose={handleCloseDialogFollower}
-                  style={{ minWidth: 300, borderRadius: 10 }}
-                >
-                  <DialogTitle
-                    id="simple-dialog-title"
-                    style={{ minWidth: 300, textAlign: "center" }}
-                  >
-                    Followings
-                  </DialogTitle>
-                  {follower.map((item, index) => {
-                    return (
-                      <Container
-                        key={index}
-                        style={{
-                          display: "flex",
-                          flexDirection: "row",
-                          padding: "4%",
-                          textAlign: "center",
-                        }}
+                        Following: {nofFollowing}
+                      </Button>
+                
+                      <Button
+                        onClick={handleClickOpenDialogFollower}
                       >
-                        <Avatar
-                          alt="r b"
-                          src=""
-                          elevation={10}
-                          style={{
-                            width: 40,
-                            height: 40,
+                        Followers: {nofFollowers}
+                      </Button>
+                    </Stack>
+                  </Grid>
 
-                            marginRight: "4%",
-                          }}
-                        >
-                          {item.substring(0, 2).toUpperCase()}
-                        </Avatar>
-                        <h3 style={{ margin: 0, marginTop: "3%" }}>{item}</h3>
-                      </Container>
-                    );
-                  })}
-                  {/* {getFollowing()} */}
-                </Dialog>
-              </Container>
-            </div>
+              </Grid>
+
+              <Grid item>
+
+
+                <Grid item xs= {12}>
+                    <Box sx={{ width: '100%' }}>
+                    <Tabs
+                      value={value}
+                      onChange={handleChange}
+                      textColor="primary"
+                      indicatorColor="primary"
+                      aria-label="secondary tabs example"
+                    >
+                      <Tab value="one" label="Shared Stories" />
+                      <Tab value="two" label="Liked Stories" />
+                    </Tabs>
+                  </Box>
+                </Grid>
+
+              </Grid>
+              
+              </Grid>
+
+                
+            </Box>
+
+
           </Container>
-          <CreatePostDialog></CreatePostDialog>
-          {postIds.length === 0 && renderEmptyPost()}
-          {postIds.length !== 0 &&
-            postIds.map((item, index) => {
-              console.log("item: ", item.id);
-              return <PostCard key={index} props={{ id: 3 }}></PostCard>;
-            })}
         </Paper>
+
       </Wrapper>
     </div>
   );
 }
 
 export default Profile;
+
+
+
+
+
+/*
+
+
+          <Container style={{ display: "flex", flexDirection: "column" }}>
+       
+            <Container style={{ display: "flex", flexDirection: "row" }}>
+              
+          <Card sx={{ minWidth: 275,display: 'flex'}} variant="outlined">
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardMedia>
+                      <Avatar
+                  alt="Remy Sharp"
+                  elevation={1}
+                >
+                  S
+                </Avatar>
+                  </CardMedia>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+               {name}
+              </Typography>
+              <Typography variant="h5" component="div">
+                About Me
+              </Typography>
+              <Typography variant="body2">
+                I am a retired railroad officer. It was always a pleasure for me to share my memories with other people. Here, you can find many of them!.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+              </Box>
+          </Card>
+
+              </Container>
+              <Container style={{ display: "flex", flexDirection: "row" }}>
+                <Button
+                  onClick={handleClickOpenDialogFollowing}
+                  style={{ margin: 10, fontSize: 15 }}
+                >
+                  Following: {nofFollowing}
+                </Button>
+                
+                <Button
+                  style={{ margin: 10, fontSize: 15 }}
+                  onClick={handleClickOpenDialogFollower}
+                >
+                  Followers: {nofFollowers}
+                </Button>
+
+              </Container>
+          </Container>
+
+          <Button color="primary" variant="contained">Edit Profile</Button>
+          <Divider/>
+          {dummyPosts.length === 0 && renderEmptyPost()}
+          {dummyPosts.length !== 0 &&
+            dummyPosts.map((item) => {
+              return  <Post post={item} curUser={"Salih Yılmaz"}></Post>;
+            })}
+
+
+
+*/
+
+
+
+/*
+
+        >
+          <Container fixed>
+
+            <Box sx={{ bgcolor: '#cfe8fc', height: '50vh' }} />
+
+            <Box sx={{ bgcolor: '#cfe8fc', height: '50vh' }} />
+
+          </Container>
+
+
+*/
