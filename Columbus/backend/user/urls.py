d8301163217615289568c9d863dcd50aef686003
kocasariumut <umut.denizli@hotmail.com>
2021-12-07 17:09:00 +0300

    post creation rest of the files are added

--- Columbus/backend/user/urls.py
+++ Columbus/backend/user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 path('logout/', views.logout.Logout.as_view()),
 path('profile_post/', views.profile_post.ProfilePost.as_view()),
+path('create_post/', views.post_create.PostCreate.as_view()),
 ]
