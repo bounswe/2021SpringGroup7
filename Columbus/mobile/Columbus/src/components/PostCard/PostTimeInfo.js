ec5859d74bee621430df202eaaffbcedaceddc76
ramazanbulut <56506578+ramazanbulut@users.noreply.github.com>
2021-12-07 14:58:34 +0300

    last changes in posting time

--- Columbus/mobile/Columbus/src/components/PostCard/PostTimeInfo.js
+++ Columbus/mobile/Columbus/src/components/PostCard/PostTimeInfo.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react'
-import { Text, View } from 'native-base'
+import React, {Component} from 'react';
+import {Text, View} from 'native-base';
 
 export class PostTimeInfo extends Component {
-   state = {
+  state = {
     data: [],
-  }
-  constructor (props) {
-    super (props);
+  };
+  constructor(props) {
+    super(props);
     this.state.data = props.data;
   }
-	render() {
-		return (
-			 <Text
-            fontSize="xs"
-            _light={{
-              color: "green.500",
-            }}
-            _dark={{
-              color: "green.400",
-            }}
-            fontWeight="500"
-            ml="-0.5"
-            mt="-1"
-          >
-            {this.state.data}
-          </Text>
-		)
-	}
+  render() {
+    return (
+      <Text
+        fontSize="xs"
+        _light={{
+          color: 'green.500',
+        }}
+        _dark={{
+          color: 'green.400',
+        }}
+        fontWeight="500"
+        ml="-0.5"
+        mt="-1">
+        {this.state.data}
+      </Text>
+    );
+  }
 }
 
-export default PostTimeInfo
+export default PostTimeInfo;
--- Columbus/mobile/Columbus/src/components/PostCard/PostingTime.js
+++ Columbus/mobile/Columbus/src/components/PostCard/PostingTime.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
-import { Text } from 'native-base'
+import React, {Component} from 'react';
+import {Text} from 'native-base';
 
 export class PostingTime extends Component {
-	state = {
+  state = {
     data: [],
-  }
-  constructor (props) {
-    super (props);
+  };
+  constructor(props) {
+    super(props);
     this.state.data = props.data;
   }
   render() {
-		return (
-			<Text
-              color="coolGray.600"
-              _dark={{
-                color: "warmGray.200",
-              }}
-              fontWeight="400"
-            >
-              {this.state.data}
-            </Text>
-            
-            
-		)
-	}
+    return (
+      <Text
+        color="coolGray.600"
+        _dark={{
+          color: 'warmGray.200',
+        }}
+        fontWeight="400">
+        {this.state.data}
+      </Text>
+    );
+  }
 }
 
-export default PostingTime
+export default PostingTime;
