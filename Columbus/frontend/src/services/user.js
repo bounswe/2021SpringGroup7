3dc05500807902c694781f1bd690365f551cae1d
mrabiabrn <merverabiabarin@gmail.com>
2021-12-11 22:34:54 +0300

    add get/set profile info

--- Columbus/frontend/src/services/user.js
+++ Columbus/frontend/src/services/user.js
@@ -4,7 +4,9 @@ const USER_SERVICE = {
   GET_PROFILE: (username) => API_INSTANCE.get(`/users/${username}/profile/`),
   GET_FOLLOWING: (username) => API_INSTANCE.get(`/users/${username}/followings/`),
   GET_FOLLOWERS: (username) => API_INSTANCE.get(`/users/${username}/followers/`),
-  GET_POSTS: (username) => API_INSTANCE.get(`/users/${username}/posts/`)
+  GET_POSTS: (username) => API_INSTANCE.get(`/users/${username}/posts/`),
+  GET_PROFILEINFO: (userId) => API_INSTANCE.get(`/user/get_profile/${userId}/`),
+  SET_PROFILEINFO: (profileInfo) => API_INSTANCE.post(`/user/set_profile/`, profileInfo),
 };
 
 export default USER_SERVICE;
