2d908ff33393348e81befd01dfc04f933c57da0d
mrabiabrn <merverabiabarin@gmail.com>
2021-12-12 22:35:28 +0300

    change accounts field

--- Columbus/frontend/src/components/Dialogs/FollowerDialog/FollowerDialog.js
+++ Columbus/frontend/src/components/Dialogs/FollowerDialog/FollowerDialog.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
@@ -8,14 +10,25 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import Divider from '@mui/material/Divider';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import PersonIcon from '@mui/icons-material/Person';
 
 import { Link } from 'react-router-dom';
 
+import USER_SERVICE from "../../../services/user"
+
+
 export default function FollowerDialog(props) {
 
     const { open, onClose, accounts, title } = props;
+
+    const [isLoading, setIsLoading] = useState([]);
+
+
+    if(isLoading){
+        return <CircularProgress color="success" />
+    }
 
     return (
     <Dialog onClose={onClose} open={open}  scroll='paper'>
@@ -23,14 +36,14 @@ export default function FollowerDialog(props) {
       <DialogContent dividers>
                     <List sx={{ pt: 0 }}>
                         {accounts.map((account) => (
-                            <>                          
-                            <ListItem button component={Link} to="/" key={account}>
+                            <>                     
+                            <ListItem button component={Link} to="/Profile" key={account}>
                                 <ListItemAvatar>
                                 <Avatar>
                                     <PersonIcon />
                                 </Avatar>
                                 </ListItemAvatar>
-                                <ListItemText primary={account} />
+                                <ListItemText primary={account['user_id__username'] || account['follow__username']} />
                             </ListItem>
                              <Divider/>
                             </>
