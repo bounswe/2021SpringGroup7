97eebee6a27c081b83b0b47c5c095654bb3369d8
Erencan Uysal <56518226+erencanuysal@users.noreply.github.com>
2021-12-12 18:28:38 +0300

    resolve base url problem

--- Columbus/frontend/src/config/api.js
+++ Columbus/frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_INSTANCE = axios.create({
-    baseURL: "http://ec2-18-197-57-123.eu-central-1.compute.amazonaws.com:8000",
+    baseURL:  process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json",
         "Authorization": localStorage.getItem('jwtToken'),
