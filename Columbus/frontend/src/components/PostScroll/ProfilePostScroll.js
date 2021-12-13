877ee9a4c0cfe262551246d1cd3c1ee7e94dd513
mrabiabrn <merverabiabarin@gmail.com>
2021-12-13 21:35:29 +0300

    Add home and profile page posts

--- Columbus/frontend/src/components/PostScroll/ProfilePostScroll.js
+++ Columbus/frontend/src/components/PostScroll/ProfilePostScroll.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-import Post from '../Post'
+import Post from '../Post/Post'
 import USER_SERVICE from "../../services/user";
 
 const useStyles = makeStyles((theme) => ({
@@ -29,55 +29,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-function PostScroll({userThatViews, userToBeViewed, ...props}) {
+function ProfilePostScroll({ userToBeViewed, userThatViews, ...props}) {
 
   const classes = useStyles();
   
-  //const { username } = props;
   const [curPosts, setCurrentPosts] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
-  const [hasMore, setHasMore] = useState(true);
-
+  //const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    console.log('hereee ')
-     USER_SERVICE.GET_PROFILEPOSTS(userToBeViewed,pageNumber,5)
+     USER_SERVICE.GET_PROFILEPOSTS(userToBeViewed,pageNumber,20)
                                             .then((res) => {
-                                              console.log('resutlll ', res.data)
-                                                //setCurrentPosts(curPosts.concat(res['data']['return']))
-                                                setIsLoading(false);
-                                                console.log('return ', res.data['return'])
-                                                setCurrentPosts(res['data']['return'])
+                                              setCurrentPosts(res.data.return)
+                                              setIsLoading(false);
                                             })
                                             .catch((error) => {
                                                 console.log(error)
                  });
     }, []);
 
-    var fetchData = () => {
-
-    console.log('hereee ')
-    USER_SERVICE.GET_PROFILEPOSTS(userToBeViewed,pageNumber,5)
-                                            .then((res) => {
-                                                if (res.data['return'].length === 0) {
-                                                      setHasMore(false);
-                                                      return;
-                                                  }
-                                                setCurrentPosts(curPosts.concat(res.data['return']))
-                                                setIsLoading(false);
-                                                console.log('return ', res.data['return'])
-                                            })
-                                            .catch((error) => {
-                                                console.log(error)
-                 });
-                      
-    setPageNumber(pageNumber+1);
-  };
-
+   
   if(isLoading) {
-    <CircularProgress color="success" />
+    return <CircularProgress color="success" />
   }
   return (<>
   {
@@ -85,24 +60,14 @@ function PostScroll({userThatViews, userToBeViewed, ...props}) {
                                 <Typography>You do not have any stories to view.</Typography>
                               <NavLink to="/Home">Explore Stories</NavLink> 
                           </Box>
-                        : <InfiniteScroll
-                              dataLength={20} // ? 20
-                              next={fetchData}
-                              hasMore={hasMore}
-                              loader={<h4>Loading...</h4>}
-                              endMessage={
-                                          <p style={{ textAlign: 'center' }}>
-                                            <b>You have seen all of your stories!</b>
-                                          </p>
-                                          } 
-                              >
+                        : <>
                               {curPosts.map((item) => {
                                                       return (
-                                                        <Post post={item} curUser={userThatViews}></Post>
+                                                        <Post key={item['story_id']} post={item} curUser={userThatViews}></Post>
                                                       );
                                                     })
                                 }
-                            </InfiniteScroll>
+                            </>
       }
       
 </>
@@ -110,7 +75,7 @@ function PostScroll({userThatViews, userToBeViewed, ...props}) {
 }
 
 
-export default PostScroll;
+export default ProfilePostScroll;
 
 
 /*
