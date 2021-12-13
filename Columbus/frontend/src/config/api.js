4119785f07ea442d91351b746eb216554bca0f33
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2021-12-13 20:03:32 +0300

    Removed static authorization header

--- Columbus/frontend/src/config/api.js
+++ Columbus/frontend/src/config/api.js
@@ -4,7 +4,6 @@ export const API_INSTANCE = axios.create({
     baseURL:  process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json",
-        "Authorization": localStorage.getItem('jwtToken'),
         Accept: "application/json",
     },
     validateStatus: (status) => status >= 200 && status < 300,
