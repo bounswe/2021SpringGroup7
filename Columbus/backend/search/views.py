3c989131355657ea77ed14ee583d8d06491b1ed0
kocasariumut <umut.denizli@hotmail.com>
2021-12-17 23:13:00 +0300

    exact text search functionality added

--- Columbus/backend/search/views.py
+++ Columbus/backend/search/views.py
@@ -87,3 +87,80 @@ class TitleExactSearch(generics.CreateAPIView):
 
 
         return JsonResponse({'return': result}, status=200)
+
+
+class TextExactSearch(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = TextExactSearchSerializer
+    #authentication_classes = [TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+
+        body = request.data
+        required_areas = {'search_text', 'page_number', 'page_size'}
+        if set(body.keys()) != required_areas:
+            return JsonResponse({'return': 'Required areas are:' + str(required_areas)}, status=400)
+
+
+        search_text = body.get('search_text')
+        page_number = body.get('page_number')
+        page_size = body.get('page_size')
+        username = body.get('username', '')
+
+        if page_number<1:
+            result = []
+            return JsonResponse({'return': result}, status=200)
+
+        stories_all = Story.objects.filter().order_by('-createDateTime')
+        stories = []
+
+        for each in stories_all:
+            similarity_value = len(set(search_text.lower().split()).intersection(set(each.text.lower().split())))
+            if similarity_value>0:
+                stories.append((similarity_value, each))
+
+
+        stories = sorted(stories, key=lambda x: x[0], reverse=True)
+        stories = [each[1] for each in stories]
+        stories = stories[(page_number-1)*page_size: page_number*page_size]
+
+        if len(stories)!=0:
+            serialized_obj = serializers.serialize('json', stories)
+            serialized_obj = json.loads(str(serialized_obj))
+
+
+            result = [each["fields"] for each in serialized_obj]
+            for i, each in enumerate(result):
+                each["owner_username"] = stories[i].user_id.username
+                each["is_liked"] = len(Like.objects.filter(story_id=stories[i], user_id__username=username))>0
+                each["story_id"] = stories[i].id
+
+                locations = Location.objects.filter(story_id=stories[i])
+                serialized_obj = serializers.serialize('json', locations)
+                serialized_obj = json.loads(str(serialized_obj))
+                serialized_obj = [each["fields"] for each in serialized_obj]
+                [each.pop('story_id', None) for each in serialized_obj]
+                each["locations"] = serialized_obj
+
+                tags = Tag.objects.filter(story_id=stories[i])
+                serialized_obj = serializers.serialize('json', tags)
+                serialized_obj = json.loads(str(serialized_obj))
+                serialized_obj = [each["fields"]["tag"] for each in serialized_obj]
+                each["tags"] = serialized_obj
+
+                try:
+                    profiles = Profile.objects.filter(user_id__username=stories[i].user_id.username)
+                    serialized_obj = serializers.serialize('json', profiles)
+                    serialized_obj = json.loads(str(serialized_obj))
+                    serialized_obj = [each["fields"]["photo_url"] for each in serialized_obj][0]
+                    each["photo_url"] = serialized_obj
+                except:
+                    each["photo_url"] = None
+
+
+
+        else:
+            result = []
+
+
+        return JsonResponse({'return': result}, status=200)
