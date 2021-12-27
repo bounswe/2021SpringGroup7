f4978a4d9d853e86b33e574a1d6fc79f5228db48
hamza.akyildiz <hamza.akyildiz@getir.com>
2021-12-27 12:14:37 +0300

    hotfix

--- Columbus/backend/user/middleware/block.py
+++ Columbus/backend/user/middleware/block.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from ..models import Blocking
 from django.http import JsonResponse
+from rest_framework.authtoken.models import Token
+
+
 
 class CheckBlock(object):
     def __init__(self, get_response):
@@ -10,19 +13,20 @@ class CheckBlock(object):
         return self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        header_token = request.META.get('HTTP_AUTHORIZATION', None)
+        if header_token:
+            token = header_token.split(' ')[1]
+            try:
+                token_obj = Token.objects.get(key=token)
+                request.user = token_obj.user
+            except Token.DoesNotExist:
+                pass
         try:
             request_owner_username = request.user
             request_owner = User.objects.get(username=request_owner_username)
-        except:
-            return
-        try:
-            user_id = view_kwargs['user_id']
-            user= User.objects.get(id=user_id)
-            if self.is_blocked(request_owner,user):
-                return JsonResponse({'response': 'Forbidden'},status=403)
-        except:
-            return
 
+        except:
+            return
         try:
             user_id = view_kwargs['user_id']
             user= User.objects.get(id=user_id)
