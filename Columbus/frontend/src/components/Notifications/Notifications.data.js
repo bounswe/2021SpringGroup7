9f5ff06e1d676cabdbbba30476202987e8ecaa1e
mrabiabrn <merverabiabarin@gmail.com>
2021-12-28 00:18:51 +0300

    Delete dummy notifications file

--- Columbus/frontend/src/components/Notifications/Notifications.data.js
+++
@@ -1,6 +0,0 @@
-export const notifications = [  {'actionTaker': 'Gandalf', 'actionType' : 'follow', 'time': '2 min ago'},
-                                {'actionTaker': 'Gandalf', 'actionType' : 'like', 'time': '1 hr ago'},
-                                {'actionTaker': 'Frodo', 'actionType' : 'follow', 'time': '2 hr ago'},
-                                {'actionTaker': 'Gandalf', 'actionType' : 'like', 'time': '7 hr ago'},
-                                {'actionTaker': 'Sam', 'actionType' : 'follow', 'time': '1 day ago'}
-]
