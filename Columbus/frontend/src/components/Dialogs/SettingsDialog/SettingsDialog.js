615d68fb75d85da54e6670ac532724bebf5f1c6b
mrabiabrn <merverabiabarin@gmail.com>
2021-12-27 21:56:00 +0300

    Fix fetch comment in post, Change update profile visibility button

--- Columbus/frontend/src/components/Dialogs/SettingsDialog/SettingsDialog.js
+++ Columbus/frontend/src/components/Dialogs/SettingsDialog/SettingsDialog.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -16,22 +17,22 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
 
+import CheckIcon from '@mui/icons-material/Check';
 import SettingsIcon from '@material-ui/icons/Settings';
 
 import USER_SERVICE from "../../../services/user";
 import MessageDialog from "../MessageDialog/MessageDialog";
 
-
 export default function SettingsDialog(props) {
 
- const { open, onClose, curProfileInfo } = props;
- const navigate = useNavigate();
+  const { open, onClose, curProfileInfo } = props;
+  const navigate = useNavigate();
 
- const [password, setPassword] = useState(null);
- const [isPublic, setIsPublic] = useState(true);
+  const [password, setPassword] = useState(null);
+  const [isPublic, setIsPublic] = useState(true);
 
- const [message, setMessage] = useState('');
- const [openMessage, setOpenMessage] = useState(false);
+  const [message, setMessage] = useState('');
+  const [openMessage, setOpenMessage] = useState(false);
 
   const handleCloseMessage = () => {
 		setOpenMessage(false);
@@ -81,83 +82,83 @@ export default function SettingsDialog(props) {
 
   return (
 
-      <Dialog 
-        open={open} 
-        onClose={onClose}
-      >
-        <DialogTitle>
-            <SettingsIcon/>
-            Settings
-        </DialogTitle>
-        <DialogContent dividers>
-         <Box sx={{ mt: 2 }}>
+    <Dialog 
+      open={open} 
+      onClose={onClose}
+    >
+      <DialogTitle>
+        <SettingsIcon/>
+        Settings
+      </DialogTitle>
 
-            <Grid container spacing={3}>
-             <Grid item xs={6} >
-                  <Stack
+      <DialogContent dividers>
+        <Box sx={{ mt: 2 }}>
+          <Grid container spacing={1}>
+            <Grid item xs={6} >
+                <Stack
                   direction="row"
-                    spacing={1}
-                   >
-                <FormControl>
-                        <InputLabel id="demo-simple-select-helper-label">Profile Visibility Options</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-helper-label"
-                            value={isPublic}
-                            label="Profile Visibility Options"
-                            onChange={handleProfileVisibility}
-                        >
-                        <MenuItem value={true}>Public</MenuItem>
-                        <MenuItem value={false}>Private</MenuItem>
-                        </Select>
-                        <FormHelperText
-                         sx={{fontSize:9}}
-                         >In private mode, only your followers can view your profile.</FormHelperText>
-                 </FormControl>
+                  spacing={1}
+                  sx={{alignItems:'center'}}
+                  >
+              <FormControl>
+                      <InputLabel id="demo-simple-select-helper-label">Profile Visibility Options</InputLabel>
+                      <Select
+                          labelId="demo-simple-select-helper-label"
+                          value={isPublic}
+                          label="Profile Visibility Options"
+                          onChange={handleProfileVisibility}
+                      >
+                      <MenuItem value={true}>Public</MenuItem>
+                      <MenuItem value={false}>Private</MenuItem>
+                      </Select>
+                      <FormHelperText
+                        sx={{fontSize:9}}
+                        >In private mode, only your followers can view your profile.</FormHelperText>
+                </FormControl>
 
-                    </Stack>
-             </Grid>
-                <Grid item xs={6} >  
-                <Button 
-                    variant = "outlined" 
-                    onClick={updateProfileVisibility} 
-                    sx={{textTransform:'none'}}>
-                        Update Visibility Mode
-                    </Button>  </Grid>
-
-              <Grid item xs={12}>
-                  <Stack
-                  direction="column"
-                    spacing={1}
-                    sx={{alignItems:'center'}}
-                    >
-                <TextField
-                    label = "Password"
-                    variant = "filled"
-                    type = "password"
-                    required value = { password }
-                    onChange = { e => setPassword(e.target.value)}
-                    /> 
-                  <Button 
-                    variant = "contained" 
-                    onClick={handleDeleteProfile} 
-                    sx={{textTransform:'none',alignItems:'center',width:150}}>
-                        Delete Profile
-                    </Button>   
                   </Stack>
+            </Grid>
+              <Grid item xs={6} >  
+              <IconButton 
+                  color = "primary" 
+                  onClick={updateProfileVisibility} 
+                  sx={{textTransform:'none'}}
+                  >
+                    <CheckIcon></CheckIcon>
+                  </IconButton>  
               </Grid>
 
+            <Grid item xs={6}>
+                <Stack
+                direction="column"
+                  spacing={1}
+                  
+                  >
+              <TextField
+                  label = "Password"
+                  variant = "filled"
+                  type = "password"
+                  required value = { password }
+                  onChange = { e => setPassword(e.target.value)}
+                  /> 
+                <Button 
+                  variant = "contained" 
+                  onClick={handleDeleteProfile} 
+                  sx={{textTransform:'none',alignItems:'center',width:150}}>
+                      Delete Profile
+                  </Button>   
+                </Stack>
             </Grid>
-            </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose}>Cancel</Button>
-        </DialogActions>
 
-        <MessageDialog open={openMessage} handleClose={handleCloseMessage} txt={message} />
-      </Dialog>
+          </Grid>
+          </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+      </DialogActions>
+
+      <MessageDialog open={openMessage} handleClose={handleCloseMessage} txt={message} />
+    </Dialog>
       
   );
 }
-
-
-//                             onChange={handleProfileVisibility}
