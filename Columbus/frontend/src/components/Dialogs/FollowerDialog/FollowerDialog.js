6149349cf4d224551102fc26294f55e9318893b2
mrabiabrn <merverabiabarin@gmail.com>
2021-12-28 14:46:13 +0300

    Fixed key names according to endpoint, Add profile photo of users

--- Columbus/frontend/src/components/Dialogs/FollowerDialog/FollowerDialog.js
+++ Columbus/frontend/src/components/Dialogs/FollowerDialog/FollowerDialog.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -13,33 +13,43 @@ import Divider from '@mui/material/Divider';
 import CircularProgress from '@mui/material/CircularProgress';
 
 import PersonIcon from '@mui/icons-material/Person';
-import USER_SERVICE from "../../../services/user"
 
 
 export default function FollowerDialog(props) {
     const { open, onClose, accounts, title } = props;
 
     return (
-    <Dialog onClose={onClose} open={open}  scroll='paper'>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent dividers>
-                    <List sx={{ pt: 0 }}>
-                        {accounts.map((account) => (
-                            <>                     
-                            <ListItem button component="a" href={'/Profile/' + (account['user_id'] || account['follow'])} key={account}>
-                                <ListItemAvatar>
-                                <Avatar>
-                                    <PersonIcon />
-                                </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary={account['user_id__username'] || account['follow__username']} />
-                            </ListItem>
-                             <Divider/>
-                            </>
-                            ))
-                        }
-
-                    </List>
+    <Dialog 
+        onClose={onClose} 
+        open={open}  
+        scroll='paper'
+    >
+        <DialogTitle>
+          {title}
+        </DialogTitle>
+        <DialogContent dividers>
+            <List sx={{ pt: 0 }}>
+                {accounts.map((account) => (
+                    <>                     
+                    <ListItem 
+                        button 
+                        component="a" 
+                        href={'/Profile/' + (account['user_id'])} 
+                        key={account}>
+                        <ListItemAvatar>
+                        <Avatar 
+                            src={account['photo_url']}>
+                            <PersonIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText 
+                            primary={account['username']} />
+                    </ListItem>
+                        <Divider/>
+                    </>
+                    ))
+                }
+            </List>
       </DialogContent>
     </Dialog>
   );
