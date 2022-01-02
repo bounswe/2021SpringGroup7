965c06959294fcf8a4eaaf027d84f437bbf3ddcb
onurcan.avci <onur.cann.avci@gmail.com>
2022-01-02 21:53:26 +0300

    CM-28 Delete redundant console

--- Columbus/mobile/Columbus/src/components/PostCard/SubComponents/PostTimeInfo.js
+++ Columbus/mobile/Columbus/src/components/PostCard/SubComponents/PostTimeInfo.js
@@ -2,23 +2,21 @@ import React, {Component} from 'react';
 import {Text, View} from 'native-base';
 
 function PostTimeInfo(props) {
-  console.log(`props`, props)
   return (
     <Text
-        fontSize="xs"
-        _light={{
-          color: 'green.500',
-        }}
-        _dark={{
-          color: 'green.400',
-        }}
-        fontWeight="500"
-        ml="-0.5"
-        mt="-1">
-        {props.data.date}
-      </Text>
+      fontSize="xs"
+      _light={{
+        color: 'green.500',
+      }}
+      _dark={{
+        color: 'green.400',
+      }}
+      fontWeight="500"
+      ml="-0.5"
+      mt="-1">
+      {props.data.date}
+    </Text>
   );
 }
 
 export default PostTimeInfo;
-
